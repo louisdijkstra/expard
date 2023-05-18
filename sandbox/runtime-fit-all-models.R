91f62883dcd4e336e49e8d2469097d7f8743459a
Louis Dijkstra <louisdijkstra@gmail.com>
2023-05-18 12:27:02 +0200

    Moved script for plotting  all true models to the expard-simulation repo

---
+++ sandbox/runtime-fit-all-models.R
@@ -0,0 +1,22 @@
+library(expard)
+library(tictoc)
+
+tic()
+
+cohort <- generate_cohort(
+  n_patients = 100,
+  simulation_time = 100,
+  n_drug_ADR_pairs = 1,
+  risk_model = "risk_model_current_use()",
+  min_chance_drug = 0.05,
+  avg_duration = 5,
+  prob_guaranteed_exposed = c(0),
+  min_chance = 0.1,
+  max_chance = .9,
+  verbose = TRUE
+)
+
+pair <- cohort[[1]]
+
+fit <- expard::fit_all_models(pair)
+toc()
