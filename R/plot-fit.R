1e5e9180f9fd9a1be55e37d94b73a58f968bc3d7
Louis Dijkstra <louisdijkstra@gmail.com>
2023-10-12 11:52:05 +0200

    Added function for plotting fit

---
+++ R/plot-fit.R
@@ -0,0 +1,51 @@
+#' Plot the Fit 
+#' 
+#' Plots the BIC for the different risk models that were fitted by the 
+#' function \code{\link{fit_all_models}}. 
+#' 
+#' @param fit The fit 
+#' @param xlab The x-label
+#' @param past If not \code{NULL}, a select number of values for past are 
+#'             used for the plot
+#' 
+#' @return ggplot 
+#' @export
+plot_fit <- function(fit, 
+                     xlab = "model", 
+                     past_values = NULL) {
+  
+  
+  
+  if('past-use(1)' %in% fit$model) {
+    simulation_time <- fit$simulation_time[1]
+    
+    past_models <- sprintf('past-use(%d)', 1:(simulation_time-1))
+    
+    fit$model <- sapply(1:nrow(fit), function(i) {
+      if(grepl('past-use', fit$model[i], fixed = TRUE)) {
+        return('past-use')
+      } else {
+        fit$model[i]
+      }
+    })
+  }
+  
+  if (!is.null(past_range)) {
+    fit <- fit %>% filter(model != 'past-use' | past %in% past_values)
+  }
+
+  best_fit <- fit %>% group_by(model) %>% 
+    filter(BIC == min(BIC))  %>% 
+    arrange(BIC)
+  
+  best_fit <- fit %>% group_by(model) %>% 
+    filter(BIC == min(BIC))  %>% 
+    arrange(BIC)
+  
+  ggplot(best_fit) +
+    geom_bar(aes(x = reorder(model, BIC), y = BIC), stat="identity") + 
+    scale_y_continuous(expand = expansion(mult = c(0, .02))) + #expand = c(0, 100)) + 
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    xlab("model") 
+}
